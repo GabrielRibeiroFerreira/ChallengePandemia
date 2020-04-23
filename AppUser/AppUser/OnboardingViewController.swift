37644a348b8e92c477363b169115ab1e3a572bb5
Luma Gabino <lumagabino@gmail.com>
2020-04-22 21:33:22 -0300

    extension to uilabel dynamically ajusts size, font re-installation

--- AppUser/AppUser/OnboardingViewController.swift
+++ AppUser/AppUser/OnboardingViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    @IBOutlet weak var forYouLabel: UILabel!
     @IBOutlet weak var professionalLabel: UILabel!
+    @IBOutlet weak var agilityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +21,19 @@ class OnboardingViewController: UIViewController {
         //Underline na palavra Profissional
         self.professionalLabel.attributedText = NSAttributedString(string: "Profissional", attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue])
         
-        
+        self.setupAccessibility()
+    }
+    
+    private func setupAccessibility() {
+        let forYouFont = UIFont(name: "SFProDisplay-Bold", size: 28) ?? UIFont.systemFont(ofSize: 28)
+        let professionalFont = UIFont(name: "SFProDisplay-Bold", size: 28) ?? UIFont.systemFont(ofSize: 28)
+        let agilityFont = UIFont(name: "SFProDisplay-Bold", size: 37) ?? UIFont.systemFont(ofSize: 37)
+        let descriptionFont = UIFont(name: "SFProDisplay-Medium", size: 18) ?? UIFont.systemFont(ofSize: 18)
+
+        self.forYouLabel.dynamicFont = forYouFont
+        self.professionalLabel.dynamicFont = professionalFont
+        self.agilityLabel.dynamicFont = agilityFont
+        self.descriptionLabel.dynamicFont = descriptionFont
     }
     
 }
