5fd68bceb848e87f63f1c41e5be852e5d5b5e99e
Luma Gabino <lumagabino@gmail.com>
2020-04-23 16:54:56 -0300

    Sobre screen autolayout and font ajusts

---
+++ AppUser/AppUser/AboutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.swift
+//  AppUser
+//
+//  Created by Luma Gabino Vasconcelos on 22/04/20.
+//  Copyright © 2020 Gabriel Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var firstDescripLabel: UILabel!
+    @IBOutlet weak var secondDescripLabel: UILabel!
+    @IBOutlet weak var contactLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController!.navigationBar.prefersLargeTitles = true
+        
+        self.setupAccessibility()
+    }
+    
+    private func setupAccessibility() {
+        let firstFont = UIFont(name: "SFProDisplay", size: 18) ?? UIFont.systemFont(ofSize: 18)
+        let secondFont = UIFont(name: "SFProDisplay-Bold", size: 18) ?? UIFont.systemFont(ofSize: 18)
+        let contactFont = UIFont(name: "SFProDisplay-Bold", size: 24) ?? UIFont.systemFont(ofSize: 24)
+        let emailFont = UIFont(name: "SFProDisplay", size: 18) ?? UIFont.systemFont(ofSize: 18)
+
+        self.firstDescripLabel.dynamicFont = firstFont
+        self.secondDescripLabel.dynamicFont = secondFont
+        self.contactLabel.dynamicFont = contactFont
+        self.emailLabel.dynamicFont = emailFont
+    }
+    
+}
