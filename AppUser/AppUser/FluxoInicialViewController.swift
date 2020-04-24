f2a1a0590b80738fa355cd1c7827e7a3decc0c80
Inara <inaratakashi@gmail.com>
2020-04-24 00:55:01 -0300

    Primeira parte telas fluxo

---
+++ AppUser/AppUser/FluxoInicialViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FluxoInicialViewController.swift
+//  AppUser
+//
+//  Created by Inara Takashi on 22/04/20.
+//  Copyright © 2020 Gabriel Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class FluxoInicialViewController: UIViewController {
+    @IBOutlet weak var titleInitial: UILabel!
+    
+    @IBOutlet weak var textInitial: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ //    self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+
+}
