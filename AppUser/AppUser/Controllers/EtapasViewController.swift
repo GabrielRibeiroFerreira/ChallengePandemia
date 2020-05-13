1c324658f12ebf3836b1ddd44b45857a11bc495e
Luma Gabino <lumagabino@gmail.com>
2020-05-13 16:26:46 -0300

    Algorithms to list all flow stages -  screen Etapas

--- AppUser/AppUser/Controllers/EtapasViewController.swift
+++ AppUser/AppUser/Controllers/EtapasViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class EtapasViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -16,6 +17,11 @@ class EtapasViewController: UIViewController {
     var stageTitles: [String] = ["Início", "Sinais de gravidade?", "Indicar Isolamento Domiciliar", "Síndrome Respiratória Aguda Grave", "Indicação para internação?", "Negativo - Lista de Procedimentos", "Positivo - Lista de Procedimentos", "Acompanhamento Leito Clínico", "Notificar e Coletar Exames", "Acompanhamento Leito de Terapia", "Notificar e Coletar Exames"]
     let appBlue = UIColor(named: "appBlue")
     let appColor = UIColor(named: "appColor")
+    
+    var etapas = [Etapa]()
+    var alternativaStack:[Etapa] = []
+    var avancarStack:[Etapa] = []
+    var titleList:[String] = []
     
     
     override func viewDidLoad() {
@@ -30,6 +36,8 @@ class EtapasViewController: UIViewController {
         self.tableView.register(nib, forCellReuseIdentifier: self.etapaCellIdentifier)
         
         self.setupNavBar()
+        
+        self.getDataFromDB()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,6 +55,72 @@ class EtapasViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = UIColor(named: "appColor")
         self.navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: self.appColor ?? UIColor.blue]
     }
+    
+    func getDataFromDB() {
+        let urlFlow = "Fluxos/idFluxo1/Etapas"
+        let refFlow = Database.database().reference().child(urlFlow)
+        refFlow.observe(.value) { (snapshot) in
+            for child in snapshot.children {
+                if let childSnapshot = child as? DataSnapshot,
+                    let dict = childSnapshot.value as? [String:Any],
+                    let tituloResumido = dict["tituloResumido"] as? String,
+                    let idEtapa = dict["idEtapa"] as? String,
+                    let id_sim = dict["id_sim"] as? String,
+                    let id_nao = dict["id_nao"] as? String,
+                    let tipo = dict["tipo"] as? String {
+                    
+                    let etapa = Etapa(tituloResumido: tituloResumido, idEtapa: idEtapa, id_sim: id_sim, id_nao: id_nao, tipo: tipo)
+                    self.etapas.append(etapa)
+                }
+            }
+            self.printTitles()
+        }
+    }
+    
+    func printTitles() {
+        var actualEtapa: Etapa
+        
+        for etapa in self.etapas {
+            if etapa.tipo == "inicial" {
+                titleList.append(etapa.tituloResumido!)
+                addProxRightList(proxEtapa: etapa.id_sim!)
+            }
+        }
+        
+        while !alternativaStack.isEmpty || !avancarStack.isEmpty {
+            if !avancarStack.isEmpty {
+                actualEtapa = avancarStack.popLast()!
+                titleList.append(actualEtapa.tituloResumido!)
+                addProxRightList(proxEtapa: actualEtapa.id_sim!)
+            }
+            else {
+                actualEtapa = alternativaStack.popLast()!
+                titleList.append(actualEtapa.tituloResumido!)
+                addProxRightList(proxEtapa: actualEtapa.id_sim!)
+                addProxRightList(proxEtapa: actualEtapa.id_nao!)
+            }
+        }
+        print(titleList)
+        stageTitles = self.titleList
+        tableView.reloadData()
+    }
+    
+    func addProxRightList(proxEtapa: String) {
+        for etapa in self.etapas {
+            if etapa.idEtapa == proxEtapa {
+                if etapa.tipo == "alternativa" {
+                    self.alternativaStack.append(etapa)
+                }
+                else if etapa.tipo == "avancar" {
+                    self.avancarStack.append(etapa)
+                }
+                else if etapa.tipo == "final" {
+                    self.titleList.append(etapa.tituloResumido!)
+                }
+            }
+        }
+    }
+    
     
     private func setupAccessibility() {
         let titleFont = UIFont(name: "SFProDisplay-Bold", size: 24) ?? UIFont.systemFont(ofSize: 24)
@@ -81,10 +155,6 @@ extension EtapasViewController: UITableViewDataSource, UITableViewDelegate {
             let titleFont = UIFont(name: "SFProDisplay-Bold", size: 18) ?? UIFont.systemFont(ofSize: 18)
             cell.titleLabel.dynamicFont = titleFont
         }
-        
-        
         return cell
     }
-    
-    
 }
