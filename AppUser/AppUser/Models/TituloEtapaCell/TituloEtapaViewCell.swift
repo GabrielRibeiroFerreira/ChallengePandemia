7b8b45a5c7ddf1a589f8b98b6d77a5aecafa737f
Luma Gabino <lumagabino@gmail.com>
2020-06-02 15:15:19 -0300

    Etapas screen

--- AppUser/AppUser/Models/TituloEtapaCell/TituloEtapaViewCell.swift
+++ AppUser/AppUser/Models/TituloEtapaCell/TituloEtapaViewCell.swift
@@ -11,8 +11,7 @@ import UIKit
 class TituloEtapaViewCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var circleView: UIImageView!
-    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var editButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,5 +27,8 @@ class TituloEtapaViewCell: UITableViewCell {
         let titleFont = UIFont(name: "SFProDisplay-Regular", size: 18) ?? UIFont.systemFont(ofSize: 18)
 
         self.titleLabel.dynamicFont = titleFont
+    }
+    @IBAction func editTitle(_ sender: Any) {
+        print("Edit clicked")
     }
 }
